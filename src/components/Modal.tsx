6846a8e01f51e5604d69e9bc06327e24e243e916
Gabriel Bessi <85580011+EyzRyder@users.noreply.github.com>
2023-09-20 08:26:13 -0300

    update modal

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,34 +1,25 @@
+import { ReactNode } from "react";
 
-export default function Modal({ modalVisible, setModalVisible }: { modalVisible:boolean, setModalVisible:(val:boolean)=>void }) {
+export default function Modal({ modalVisible, setModalVisible, children }: { modalVisible: boolean, setModalVisible: Function, children: ReactNode }) {
   return (
     <>
-        <button onClick={() => setModalVisible(true)}>
-          open
-          {/* <Icon name="more-vertical" size={32} color="#FFF" /> */}
-        </button>
-        {modalVisible &&  <div className="">
-          <div className='flex-1'>
-            <div className='flex-row items-center justify-between px-3 py-3 border-b-2 border-[#ddd]'>
+      {modalVisible &&
+        <>
+          <div onClick={() => setModalVisible(false)} className="z-[1] top-0 left-0 flex flex-col bg-gray-500/75 min-w-full min-h-screen absolute"></div>
+          <div className='z-[2] min-w-[500px] bg-white flex-1 flex flex-col absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>
+            <div className='flex  flex-row items-center justify-between px-3 py-3 border-b-2 border-[#ddd]'>
               <button
                 onClick={() => setModalVisible(false)}
               >
-                back
+                Voltar
                 {/* <Icon name='chevron-left' size={30} color={'#2A416F'} /> */}
               </button>
               <p className='font-title text-lg text-[#2A416F]'>Opções</p>
             </div>
-            <div className="flex-1">
-              <button
-                onClick={() => { signOut(auth); router.back() }}
-                className={`flex-row items-center justify-between border-b-2 border-[#ddd] p-3 `}
-              >
-                <p className='text-sm text-[#555]'>
-                  Log Out
-                </p>
-              </button>
-            </div>
+            {children}
           </div>
-          </div>}
         </>
+      }
+    </>
   )
 }
--- src/pages/Base.tsx
+++ src/pages/Base.tsx
@@ -1,10 +1,28 @@
 import { IonContent } from '@ionic/react'
 import { useState } from 'react';
+import Modal from '../components/Modal';
+import { signOut } from 'firebase/auth';
+import { auth } from '../lib/firebaseConfig';
 
 export default function Base() {
   const [modalVisible, setModalVisible] = useState(false);
-
   return (
-    <IonContent>Base</IonContent>
+    <IonContent>
+      <button onClick={() => setModalVisible(true)}>
+        open
+      </button>
+      <Modal modalVisible={modalVisible} setModalVisible={setModalVisible}>
+        <div className="flex-1">
+          <button
+            onClick={() => { signOut(auth); }}
+            className={`flex-row items-center justify-between border-b-2 border-[#ddd] p-3 `}
+          >
+            <p className='text-sm text-[#555]'>
+              Log Out
+            </p>
+          </button>
+        </div>
+      </Modal>
+    </IonContent>
   )
 }
