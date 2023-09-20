6d14c1432c609af11a832b8447b94c44efabb5e9
Gabriel Bessi <85580011+EyzRyder@users.noreply.github.com>
2023-09-20 09:23:35 -0300

    update modal

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -6,7 +6,7 @@ export default function Modal({ modalVisible, setModalVisible, children }: { mod
       {modalVisible &&
         <>
           <div onClick={() => setModalVisible(false)} className="z-[1] top-0 left-0 flex flex-col bg-gray-500/75 min-w-full min-h-screen absolute"></div>
-          <div className='z-[2] min-w-[500px] bg-white flex-1 flex flex-col absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>
+          <div className='z-[2] w-[80%] bg-white flex-1 flex flex-col absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>
             <div className='flex  flex-row items-center justify-between px-3 py-3 border-b-2 border-[#ddd]'>
               <button
                 onClick={() => setModalVisible(false)}
