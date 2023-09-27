2e969e19fcb41647d812afa33864a0a9396842a5
Gabriel Bessi <85580011+EyzRyder@users.noreply.github.com>
2023-09-27 18:35:34 -0300

    dependencie update

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -4,9 +4,11 @@ import { X } from "lucide-react"
 
 import { cn } from "@/lib/utils"
 
-const Dialog = DialogPrimitive.Root
+const Dialog = DialogPrimitive.Root;
 
-const DialogTrigger = DialogPrimitive.Trigger
+const DialogTrigger = DialogPrimitive.Trigger;
+
+interface type extends DialogPrimitive.DialogPortalProps, React.HTMLAttributes<HTMLDivElement>{};
 
 const DialogPortal = ({
   className,
