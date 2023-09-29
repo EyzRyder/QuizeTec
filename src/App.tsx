74fd960dd5ff8c628a5a6db4cd2280c2c3c8379d
Gabriel Bessi <85580011+EyzRyder@users.noreply.github.com>
2023-09-28 22:29:39 -0300

    build bug fix

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ import "@ionic/react/css/display.css";
 /* Theme variables */
 import "./theme/variables.scss";
 import "./themes/globals.scss";
-import { router } from "./Router";
+// import { router } from "./Router";
 import Routing from "./Routes";
 
 setupIonicReact();
