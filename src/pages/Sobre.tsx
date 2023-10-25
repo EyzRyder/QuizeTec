6c42485ea9e092b3570c37e4cf20c79e5cf5356f
Gabriel Bessi <85580011+EyzRyder@users.noreply.github.com>
2023-10-25 13:35:54 +0000

    fix h sobre

--- src/pages/Sobre.tsx
+++ src/pages/Sobre.tsx
@@ -21,6 +21,8 @@ export default function Sobre() {
         animate={{ opacity: 1, scale: 1 }}
         exit={{ opacity: 0, scale: 0.8 }}
         transition={{ duration: 1 }}
+        className="h-full "
+
       >
         <div className="flex flex-col flex-1 h-[105vh] items-center bg-[#F5F9FC]">
           <div className="flex flex-row pt-10 w-full justify-between items-center">
